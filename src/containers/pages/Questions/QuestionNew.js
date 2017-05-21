ab140848415b2f1fdb89acf4fbacad816fb492fa
Peter Absolon <peterabsolon@yahoo.com>
2017-05-21 16:47:24 +0200

    Fix name case inconsistencies

---
+++ src/containers/pages/Questions/QuestionNew.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import { QuestionNewForm } from 'containers/forms/questions'
+
+const QuestionNew = props => <QuestionNewForm {...props} />
+
+export default QuestionNew
